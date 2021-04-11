3aa90a2aec97b6ae4b0b5fcccc13e9226a14adad
Maximilian Bethke <git@maxbethke.de>
2021-04-11 13:09:43 +0200

    refactoring and wrote new failing testcase

--- src/fizzBuzz.php
+++ src/fizzBuzz.php
@@ -7,11 +7,7 @@ require_once("src/models/invalidInputException.php");
 use FizzBuzz\Exceptions\InvalidInputException;
 
 class FizzBuzz {
-    public function convertChar($char): string {
-        if(!is_int($char)) {
-            throw new InvalidInputException("Can only FizzBuzz integers!");
-        }
-        
-        return strval($char);
+    public function convertChar(int $char): string {  
+        return (string)$char;
     }
 }
--- tests/functionalTest.php
+++ tests/functionalTest.php
@@ -10,7 +10,24 @@ use FizzBuzz\Exceptions\InvalidInputException;
 use FizzBuzz\App\FizzBuzz;
 
 class functionalTest extends TestCase {
-    public function testConvertCharReturnsString() {
+    const TEST_UP_TO = 100;
+    
+    private $numbersFizz;
+    private $numbersBuzz;
+    
+    private function getNumbersDivisibleBy(int $subject, int $max): array {
+        $out = [];
+        
+        for($num = 1; $num <= $max; $num++) {
+            if($num % $subject === 0) {
+                $out[] = $num;
+            }
+        }
+
+        return $out;
+    }
+
+    public function testConvertCharReturnsString(): void {
         $fizzBuzz = new FizzBuzz();
         $charToCheck = rand(1, 100);
 
@@ -21,11 +38,26 @@ class functionalTest extends TestCase {
         );
     }
 
-    public function testDoesOnlyAcceptNumbers() {
+    public function testDoesOnlyAcceptNumbers(): void {
         $fizzBuzz = new FizzBuzz();
         $validationString = random_bytes(3);
 
-        $this->expectException(InvalidInputException::class);
+        $this->expectException(\TypeError::class);
         $fizzBuzz->convertChar($validationString);
+    }
+
+    public function testReturnsFizz(): void {
+        if(!$this->numbersFizz) {
+            $this->numbersFizz = $this->getNumbersDivisibleBy(3, self::TEST_UP_TO);
+        }
+        
+        $fizzBuzz = new FizzBuzz();
+
+        foreach($this->numbersFizz as $num) {
+            $this->assertEquals(
+                "Fizz",
+                $fizzBuzz->convertChar($num)
+            );
+        }
     }
 }
